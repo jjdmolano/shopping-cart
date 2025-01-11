d7c44a2d464a87dcb208cf6ab9fca007517c326e
Johann Molano <jjdmolano@gmail.com>
2025-01-12 02:43:29 +0800

    updated file structure

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import { processItems } from "./modules/processItems.js";
 
+// test data import to simulate data coming from the front-end
+import testData from "./db/scenarios.json" with { type: "json" };
+// test data divided into scenarios
+const { scenario1, scenario2, scenario3, scenario4 } = testData;
+
 export function handler(data) {
   processItems(data);
 }
--- modules/processItems.js
+++ modules/processItems.js
@@ -1,12 +1,5 @@
 import { ShoppingCart } from "./modules/classes.js";
 import { pricingRules } from "./modules/pricingRules.js";
-import catalog from "../db/products.json" with { type: "json" };
-const { item0 } = catalog;
-
-// test data import to simulate data coming from the front-end
-import testData from "./db/scenarios.json" with { type: "json" };
-// test data divided into scenarios
-const { scenario1, scenario2, scenario3, scenario4 } = testData;
 
 export function processItems(data) {
   const cart = new ShoppingCart();
@@ -15,11 +8,11 @@ export function processItems(data) {
   if (data.items.length > 0) {
     data.forEach(item => addItem(cart, item));
   }
-  
+
   if (data.promoCode) {
     cart.add(item0, data.promoCode);
   }
-  
+
   lastStep(cart);
 }
 
